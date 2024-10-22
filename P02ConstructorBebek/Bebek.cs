0ae261599bced1794a0ac4ceb8fee317f9aa6cae
izzetcosar <izzetcosar@gmail.com>
2024-10-23 02:35:03 +0300

    P02ConstructorBebek eklendi.

---
+++ P02ConstructorBebek/Bebek.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02ConstructorBebek
+{
+    public class Bebek //istenen 3 adet property olusturuldu.
+    {
+        public string _name {  get; set; }
+        public string _lastName { get; set; }
+        public DateTime _date { get; set; }
+
+        public Bebek() //Parametre almayan bebek
+        {
+            Console.WriteLine("Ingaaaa");
+            _date = DateTime.Now; //Olusturma anında doğum tarihi olusturmak için anlık tarihi atadım.
+        }
+
+        public Bebek(string name, string lastName) //ad ve soyad paremetrelerini alan bebek
+        {
+            Console.WriteLine("Ingaaaa");
+            _date = DateTime.Now; //Olusturma anında doğum tarihi olusturmak için anlık tarihi atadım.
+            _name = name;
+            _lastName = lastName;
+            
+        }
+        public void kimlikyazdir() //Konsol ekranına bebeklerimizin kimliklerini yazdırıcak metotu olusturdum.
+        { 
+            Console.WriteLine($"Bebek adı:{_name}{_lastName},Doğum Tarihi:{_date}");
+        }
+
+
+
+        
+
+
+    }
+}
---
+++ P02ConstructorBebek/Program.cs
@@ -0,0 +1,9 @@
+﻿using P02ConstructorBebek;
+
+Bebek bebek = new Bebek("Nina", "Sarapova"); //isim ve soyismini parametre alan bebeği olusturduk.
+bebek.kimlikyazdir(); //Metot kullanıldı.
+Bebek bebek1 = new Bebek(); //parametreleri newleyerek olusturduğumuz bebek
+bebek1._name = "Annie";
+bebek1._lastName = "Yakarow";
+bebek1.kimlikyazdir(); //metot kullanıldı
+Console.ReadKey();
