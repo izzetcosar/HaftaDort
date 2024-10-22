f15ff2cafaac5b2ee2b5a78a1da52b87bdbcf601
izzetcosar <izzetcosar@gmail.com>
2024-10-23 01:02:16 +0300

    Add project files.

---
+++ P01IlkSinifimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,24 @@
+﻿using P01IlkSinifimiziOlusturuyoruz;
+
+
+person Ogrenci1 = new person();
+Ogrenci1.name = "izzet";
+Ogrenci1.lastname = "cosar";
+Ogrenci1.dateTime = new DateTime(1994, 10, 12);
+
+person Ogrenci2 = new person();
+Ogrenci2.name = "buket";
+Ogrenci2.lastname = "engel";
+Ogrenci2.dateTime = new DateTime(1998, 4, 4);
+
+person Ogretmen1 = new person();
+Ogretmen1.name = "Gürkan";
+Ogretmen1.lastname = "Gümüş";
+Ogretmen1.dateTime = new DateTime(1984, 5, 24);
+
+
+Ogrenci1.KendiniTanit();
+Ogrenci2.KendiniTanit();
+Ogretmen1.KendiniTanit();
+
+Console.ReadKey();
---
+++ P01IlkSinifimiziOlusturuyoruz/person.cs
@@ -0,0 +1,37 @@
+﻿
+namespace P01IlkSinifimiziOlusturuyoruz
+{
+    public class person
+    {
+        public string name; //field kısmı öğrenci veya öğretmenin ismi soyismi ve doğum tarihini oluşturuyoruz.
+        public string lastname;
+        public DateTime dateTime;
+
+        public string Name
+        {
+            get { return name; }
+            set {  name = value; }
+        }
+        public string Lastname
+        { 
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public DateTime DateTime
+        {
+            get { return dateTime; }
+            set
+            {
+                dateTime = value;
+            }
+        }
+
+        public void KendiniTanit() //Kullanıcının ismini soyismini ve dogum tarihini yazdırdığımız bir metot oluşturduk.
+        {
+            Console.WriteLine($"Merhaba benim adım {name.ToUpper()} {lastname.ToUpper()} ve doğum tarihim {dateTime.ToShortDateString()}'dir.");
+        }
+    }
+    
+}
+
+
