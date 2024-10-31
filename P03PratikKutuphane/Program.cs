7b61e0f99257f75b27db80230883dede8e854ccb
izzetcosar <izzetcosar@gmail.com>
2024-11-01 00:56:16 +0300

    P03PratikKutuphane eklendi.

---
+++ P03PratikKutuphane/Program.cs
@@ -0,0 +1,25 @@
+﻿using P03PratikKutuphane;
+
+//Parametresiz constructor ile kitap oluşturma
+Kitap kitap1 = new Kitap();
+kitap1.Ad = "Adı Aylin";
+kitap1.YazarAdi = "Ayşe";
+kitap1.YazarSoyadi = "Kulin";
+kitap1.SayfaSayisi = 398;
+kitap1.YayinEvi = "Remzi Kitabevi";
+kitap1.KitapBilgileri();
+
+Console.WriteLine("----------------------------");
+//Parametreli constructor ile kitap oluşturma
+Kitap kitap2 = new Kitap("Adı Aylin", "Ayşe","Kulin",398, "Remzi Kitabevi");
+kitap2.KitapBilgileri();
+
+//Class:Birbirleriyle aynı özelliklere sahip nesnelerin ortak şemada topladığımız alanlardır.Örneğimizde Kitap sınıfı oluşturduk.
+//Her kitabın adı yazarı sayfasayısı ve yayınlandıysa bir yayınevi mevcuttur.
+
+//Property: Sınıf içinde oluşturduğumuz özelliklerdir.(kitap adı,Yazar Adı,Yazar soyadı,sayfa sayısı,Yayın evi)
+
+//New: Olusturduğumuz class özelliklerine ait yeni bir nesne oluşturmak için kullanuyoruz.(Kitap1,Kitap2)
+
+//Constructor:Yeni bir nesne oluştururken kullanılan metottur.Bunlar nesnelere ilk değerlerini verir.
+Console.ReadLine();
---
+++ P03PratikKutuphane/kitap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03PratikKutuphane
+{
+    public class Kitap //Class Özelliklerini oluşturduğumuz property
+    {
+        public string Ad {  get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string YayinEvi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+        public Kitap()  //Parametresiz constructor parametrelerini oluşturduğumuzda atayacağız
+        { 
+        KayitTarihi = DateTime.Now; //Kayıt Tarihi otomatik olarak kitap oluşturulduğunda oluşturuluyor
+        }
+        public Kitap(string ad, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinEvi) // Parametreli Constructor oluştururken tüm bilgileri doldurmamız gerekiyor
+        {
+            Ad = ad;
+            YazarAdi = yazarAdi;
+            YazarSoyadi = yazarSoyadi;
+            SayfaSayisi = sayfaSayisi;
+            YayinEvi = yayinEvi;
+            KayitTarihi = DateTime.Now; //Kayıt Tarihi otomatik olarak kitap oluşturulduğunda oluşturuluyor
+        }
+        public void KitapBilgileri() //bilgileri yazdırdığımız method
+        {
+            Console.WriteLine($"Kitap Adı: {Ad}");
+            Console.WriteLine($"Yazar Adı:{YazarAdi} {YazarSoyadi}");
+            Console.WriteLine($"Sayfa Sayısı: {SayfaSayisi}");
+            Console.WriteLine($"Yayınevi: {YayinEvi}");
+            Console.WriteLine($"Kayıt Tarihi: {KayitTarihi}");
+        }
+
+    }
+}
