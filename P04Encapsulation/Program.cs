7b2f9359f8fb5d8e0034bfab750cd2fe98b0c9cc
izzetcosar <izzetcosar@gmail.com>
2024-11-01 02:28:52 +0300

    P04Encapsulation eklendi.

---
+++ P04Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using P04Encapsulation;
+
+araba araba1 = new araba();
+araba1.marka = "Mercedes-Benz";
+araba1.model = "C180";
+araba1.renk = "İnci Beyazı";
+araba1.kapiSayisi = 4;
+araba1.ArabaBilgileriniGöster();
+Console.WriteLine();
+
+araba araba2 = new araba();
+araba1.marka = "BMW";
+araba1.model = "M3";
+araba1.renk = "Parlement Mavisi";
+araba1.kapiSayisi = 2;
+araba1.ArabaBilgileriniGöster();
+Console.WriteLine();
+
+araba araba3 = new araba();
+araba1.marka = "Dacia";
+araba1.model = "Duster";
+araba1.renk = "Siyah";
+araba1.kapiSayisi = 1;
+araba1.ArabaBilgileriniGöster();
+
+Console.ReadKey();
---
+++ P04Encapsulation/araba.cs
@@ -0,0 +1,50 @@
+﻿namespace P04Encapsulation
+{
+    public class araba //class oluşturduk.
+    {
+        public string marka { get; set; } //properies
+
+        public string model { get; set; }
+
+        public string renk { get; set; }
+
+        private int _kapiSayisi; //kapı sayısında istendiği şekilde kapsülleme yapıyoruz.
+
+        public int kapiSayisi
+        {
+            get
+            {
+                return _kapiSayisi;
+            }
+            set
+            {
+                if (value == 2 || value == 4) //kapı sayısı 2 veya 4 girilmesi durumunda kapı sayısını gösteriyoruz
+                {
+                    _kapiSayisi = value;
+                }
+                else
+                {
+                    _kapiSayisi = -1; //kapı adedi istenen dışında girildiğinde otomatik olarak -1 girişi yapıyoruz.
+                }
+            }
+        }
+
+        public void ArabaBilgileriniGöster() //araba özelliklerini gösteren bir metod oluşturduk.
+        {
+            Console.WriteLine($"Marka: {marka}");
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Renk: {renk}");
+            if (_kapiSayisi == -1) //kapı sayısı istenen sayı dışında girildiğinde -1 gösterilip uyarı veriliyor.
+            {
+                Console.WriteLine($"Kapı Sayısı: {kapiSayisi} (Uyarı: Kapı sayısı sadece 2 veya 4 olabilir.)");
+            }
+            else
+            {
+                Console.WriteLine($"Kapı Sayısı: {kapiSayisi}"); //kapı sayısı istenen şekilde girildiğinde adet bilgisi veriliyor.
+            }
+            
+
+        }
+    }
+}
+    
