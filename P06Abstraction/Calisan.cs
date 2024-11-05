9883ba2058191024a5172182c6ce2420d0c202ca
izzetcosar <izzetcosar@gmail.com>
2024-11-05 15:31:10 +0300

    abstraction eklendi.

---
+++ P06Abstraction/Calisan.cs
@@ -0,0 +1,63 @@
+﻿namespace P06Abstraction
+{
+    public abstract class Calisan
+    {
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+        
+
+        public Calisan(string ad, string soyad,string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Departman = departman;
+           
+            
+        }
+        public abstract void Gorev();
+
+        public void kimlik()
+        {
+            Console.Write($"{Ad} {Soyad},{Departman} departmanında ");
+        }
+
+        
+    }
+    public class YazilimGelistirici : Calisan
+    {
+        public YazilimGelistirici(string ad, string soyad, string departman) : base(ad, soyad, departman)
+        {
+        }
+
+        public override void Gorev()
+        {
+            Console.Write("yazılım geliştirici olarak çalışıyorum.");
+            
+        }
+    }
+    class ProjeYoneticisi : Calisan
+    {
+        public ProjeYoneticisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine("Proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+
+    
+    class SatisTemsilcisi : Calisan
+    {
+        public SatisTemsilcisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine("Satış temsilcisi olarak çalışıyorum.");
+        }
+    }
+
+}
---
+++ P06Abstraction/Program.cs
@@ -0,0 +1,15 @@
+﻿using P06Abstraction;
+using System.Security.Cryptography;
+
+YazilimGelistirici yazilimci1 = new YazilimGelistirici("hasan", "çıldırmış", "yazılım geliştirme");
+
+yazilimci1.kimlik();
+yazilimci1.Gorev();
+Console.WriteLine();
+ProjeYoneticisi projeYoneticisi1 = new ProjeYoneticisi("nermin", "kandırmış","Proje Yönetimi");
+projeYoneticisi1.kimlik();
+projeYoneticisi1.Gorev();
+
+
+Console.ReadKey();
+
