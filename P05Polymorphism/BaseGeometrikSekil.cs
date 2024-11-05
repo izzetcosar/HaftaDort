1dbb0f3b1bb6dc2b7292f7e6c3cd884d76b40e33
izzetcosar <izzetcosar@gmail.com>
2024-11-05 11:14:54 +0300

    P05Polymorphism

---
+++ P05Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,73 @@
+﻿// BaseGeometrikSekil sınıfı
+public class BaseGeometrikSekil
+{
+    public double Genişlik { get; set; }
+    public double Yükseklik { get; set; }
+
+    public BaseGeometrikSekil(double genişlik, double yükseklik)
+    {
+        Genişlik = genişlik;
+        Yükseklik = yükseklik;
+    }
+
+    // Polimorfizm için sanal alan hesaplama metodu
+    public virtual double AlanHesapla()
+    {
+        return Genişlik * Yükseklik; // Default olarak Kare ve Dikdörtgenler için
+    }
+    public virtual void AlanYazdir()
+    {
+        Console.WriteLine($"Alan: {AlanHesapla()}");
+    }
+}
+
+// Kare sınıfı
+public class Kare : BaseGeometrikSekil
+{
+    public Kare(double kenar) : base(kenar, kenar)
+    {
+    }
+
+    public override double AlanHesapla()
+    {
+        return Genişlik * Yükseklik; // Kare olduğu için Genişlik x Yükseklik, yani kenar^2
+    }
+    public override void AlanYazdir()
+    {
+        Console.WriteLine($"Kare Alanı: {AlanHesapla()}");
+    }
+}
+
+
+public class Dikdortgen : BaseGeometrikSekil
+{
+    public Dikdortgen(double genişlik, double yükseklik) : base(genişlik, yükseklik)
+    {
+    }
+
+    public override double AlanHesapla()
+    {
+        return Genişlik * Yükseklik; // Dikdörtgen alanı = Genişlik x Yükseklik
+    }
+    public override void AlanYazdir()
+    {
+        Console.WriteLine($"Dikdörtgen Alanı: {AlanHesapla()}");
+    }
+}
+
+
+public class DikUcgen : BaseGeometrikSekil
+{
+    public DikUcgen(double genişlik, double yükseklik) : base(genişlik, yükseklik)
+    {
+    }
+
+    public override double AlanHesapla()
+    {
+        return (Genişlik * Yükseklik) / 2; // Dik üçgen alanı = (Genişlik x Yükseklik) / 2
+    }
+    public override void AlanYazdir()
+    {
+        Console.WriteLine($"Dik Üçgen Alanı: {AlanHesapla()}");
+    }
+}
---
+++ P05Polymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿// Kare nesnesi
+BaseGeometrikSekil kare = new Kare(5);
+kare.AlanYazdir();  
+
+// Dikdörtgen nesnesi
+BaseGeometrikSekil dikdortgen = new Dikdortgen(4, 6);
+dikdortgen.AlanYazdir();  
+
+// Dik üçgen nesnesi
+BaseGeometrikSekil dikUcgen = new DikUcgen(3, 7);
+dikUcgen.AlanYazdir();  
+Console.ReadKey();
